533cce99660b4fc19db368c7dfd10e231241423c
Srinivas Guddati <sguddati@sapient.com>
2014-04-14 12:21:48 +0530

    sample panera contact form

---
+++ form-sample-content.php
@@ -0,0 +1,46 @@
+		<section class="text-banner">
+			<header class="heading"><h2>Panera Contact Form</h2></header>
+		</section>
+		<div class="content-inner-wrapper email-signup">
+			<div class="content-inner">
+                <div class="main-content text-padding">
+                    <section class="group">
+						<article id="two-column-space" class="two-column-large">
+							<h2>Please enter your information</h2>
+								<form action="investor-relations-email-signup-confirmation.php" id="sample-form" class="customButton customInputOptions" method="post">
+									<table class="user-form input-form-table">
+                                    <tr>
+											<td><label for="first_name">First Name </label></td>
+										</tr>
+										<tr>
+											<td><input type="text" id="first_name" name="first_name" value="" data-rule="required|legalname" data-msg-required="Required" data-msg-legalname="Invalid" /></td>
+										</tr>
+                                        <tr>
+											<td><label for="last_name">Last Name </label></td>
+										</tr>
+										<tr>
+											<td><input type="text" id="last_name" name="last_name" value="" data-rule="required|legalname" data-msg-required="Required" data-msg-email="Invalid" /></td>
+										</tr>
+                                         <tr>
+											<td><label for="user_email">Email </label></td>
+										</tr>
+										<tr>
+											<td><input type="email" id="user_email" name="user_email" data-rule="required|email"  data-msg-required="Required" data-msg-email="Invalid Email"/></td>
+										</tr>
+                                        <tr>
+											<td><label for="user_email">Confirm Email </label></td>
+										</tr>
+										<tr>
+											<td><input type="email" id="user_email" name="user_email" data-rule="required|email|equalTo(#join_email)"  data-msg-required="Required" data-msg-email="Invalid Email"/></td>
+										</tr>
+                                        <tr>
+											<td class="form-submit-button"><button type="submit" class="button-primary">Send Request<span class="cta"></span></button></li></td>
+										</tr>
+										
+							        </table>
+								</form>
+					</article>
+                </section>
+            </div>
+        </div>
+    </div>
